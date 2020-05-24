7c37bcccad0900da9d12422628dab07f582a2099
wbbbbbz <wbbbbbz@gmail.com>
2020-05-24 21:41:09 +0900

    05-Hyper-Parameters

--- 04-kNN/04-Accuracy-Score/playML/kNN.py
+++ 04-kNN/04-Accuracy-Score/playML/kNN.py
@@ -2,15 +2,20 @@ import numpy as np
 from math import sqrt
 from collections import Counter
 from .metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
 
 class KNNClassifier:
 
-    def __init__(self, k):
+    def __init__(self, k=5, weights='uniform'):
         """初始化kNN分类器"""
         assert k >= 1, "k must be valid"
+        assert weights in ['uniform', 'distance']
         self.k = k
         self._X_train = None
         self._y_train = None
+        self.weights = weights
 
     def fit(self, X_train, y_train):
         """根据训练数据集X_train和y_train训练kNN分类器"""
@@ -26,9 +31,9 @@ class KNNClassifier:
     def predict(self, X_predict):
         """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
         assert self._X_train is not None and self._y_train is not None, \
-                "must fit before predict!"
+            "must fit before predict!"
         assert X_predict.shape[1] == self._X_train.shape[1], \
-                "the feature number of X_predict must be equal to X_train"
+            "the feature number of X_predict must be equal to X_train"
 
         y_predict = [self._predict(x) for x in X_predict]
         return np.array(y_predict)
@@ -41,11 +46,20 @@ class KNNClassifier:
         distances = [sqrt(np.sum((x_train - x) ** 2))
                      for x_train in self._X_train]
         nearest = np.argsort(distances)
-
         topK_y = [self._y_train[i] for i in nearest[:self.k]]
-        votes = Counter(topK_y)
 
-        return votes.most_common(1)[0][0]
+        if self.weights == 'uniform':
+
+            votes = Counter(topK_y)
+
+            return votes.most_common(1)[0][0]
+        else:
+            categories = np.arange(10)
+            weights = np.zeros(10)
+            for x in np.arange(len(topK_y)):
+                weights[topK_y[x]] += 1 / distances[nearest[x]]
+
+            return categories[np.argsort(weights)[-1]]
 
     def score(self, X_test, y_test):
         """根据测试数据集 X_test 和 y_test 确定当前模型的准确度"""
@@ -55,5 +69,3 @@ class KNNClassifier:
 
     def __repr__(self):
         return "KNN(k=%d)" % self.k
-
-
